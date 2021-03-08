6f75e9ac620ba1d866da839658119f3731bf06d4
Ngo, Brandon <brandon.ngo@tcfbank.com>
2021-03-08 13:57:37 -0600

    working state with new paramters, working state that prints the actual data (using json)

--- cmc-api-price.py
+++ cmc-api-price.py
@@ -1,15 +1,19 @@
 import requests
 
 # https://coinmarketcap.com/api/documentation/v1/#section/Quick-Start-Guide
-#https://coinmarketcap.com/api/documentation/v1/#operation/getV2CryptocurrencyOhlcvHistorical
+# https://coinmarketcap.com/api/documentation/v1/#operation/getV2CryptocurrencyOhlcvHistorical
 # Latest lists of all coins from coinmarketcap api key
 
-cmc_listings_url = (
-    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
-)
+cmc_listings_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 # Limit the data returned to first 20 cryptocurrencies using query parameters
 
-parameters = {"start": "1", "limit": "10", "convert": "USD"}
+parameters = {
+    "start": "1",
+    "limit": "1",
+    "convert": "USD",
+    "sort": "market_cap_by_total_supply_strict",
+    "sort": "market_cap_strict"
+}
 # Pass the required API key
 
 headers = {
@@ -20,3 +24,6 @@ headers = {
 
 response = requests.get(cmc_listings_url, headers=headers, params=parameters)
 # Convert the json payload to a python data type
+
+#Need to print .json() bc response doesn't have a get method. Need to use json to get actual data
+print(response.json())

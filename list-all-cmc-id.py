03b736800ec5b8af6fe7a64a028fcdcae88c860b
Ngo, Brandon <brandon.ngo@tcfbank.com>
2021-03-08 15:39:45 -0600

    updated w/ questions

--- list-all-cmc-id.py
+++ list-all-cmc-id.py
@@ -4,23 +4,17 @@ import requests
 # https://coinmarketcap.com/api/documentation/v1/#operation/getV2CryptocurrencyOhlcvHistorical
 # Latest lists of all coins from coinmarketcap api key
 
-cmc_listings_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+cmc_listings_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map"
 # Limit the data returned to first 20 cryptocurrencies using query parameters
 
 parameters = {
-    "start": "1",
-    "limit": "1",
-    "convert": "USD",
-    "sort": "market_cap_by_total_supply_strict",
-    "sort": "market_cap_strict"
-    #"id":"3945" #ID doesn't work here, bc not part of paramters in docs. Maybe try in header?
+    "symbol": "ONE",
 }
 # Pass the required API key
 
 headers = {
     "Accepts": "application/json",
     "X-CMC_PRO_API_KEY": "00262ed9-c562-4378-b9e6-efeff1b62b97",
-    "name":"Ethereum"
 }
 # Call the API endpoint and pass parameters and header information
 
@@ -30,13 +24,5 @@ response = requests.get(cmc_listings_url, headers=headers, params=parameters)
 #Need to print .json() bc response doesn't have a get method. Need to use json to get actual data
 print(response.json())
 
-"""Stuck..
-QUESTION: How do I input a coin that I want to view- How to view coin named "Harmony"? The ID is 3945, but I can't figure out how to put id in the parameter?
-
-QUESTION: on https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest
-What does sort against ... mean?
-What is the difference between that and the parameter- why can't I put that the sort against, specifically "symbol" in the parameter?
-
-
-
-"""
+#Harmony "id":3945
+#ETH: id 1027
